2e6c01dbd670abc240086b8b7d0f631e6e264c91
Jennifer Smith <jennifer@home>
2013-06-10 20:51:51 +0100

    New alto 2

--- src/leipzig/example/row_row_row_your_boat.clj
+++ src/leipzig/example/row_row_row_your_boat.clj
@@ -21,6 +21,12 @@
 (defmethod play-note :bass [note]
   (pick 0.9 0.2 (update-in note [:pitch] #(- % 12))))
 
+(defmethod play-note :sop1 [note]
+  (pick 0.6 0.4 note))
+
+(defmethod play-note :alto1 [note]
+  (pick 0.5 0.6 note))
+
 (def melody "A simple melody built from durations and pitches."
                ; Row, row, row your boat,
   (->> (phrase [3/3 3/3 2/3 1/3 3/3]
@@ -64,7 +70,7 @@
     ;;            ; Life is but a dream!
     ;;    (phrase [2/3 1/3 2/3 1/3 6/3] 
     ;;            [  4   3   2   1   0]))
-    (where :part (is :leader))))
+    (where :part (is :sop1))))
 (def countermelody-ode 
                
   (->> (phrase [1/4 1/4 1/4 1/4]
@@ -90,7 +96,34 @@
     ;;            ; Life is but a dream!
     ;;    (phrase [2/3 1/3 2/3 1/3 6/3] 
     ;;            [  4   3   2   1   0]))
-    (where :part (is :leader))))
+    (where :part (is :alto1))))
+
+(def countermelody-ode-alto2
+               
+  (->> (phrase [1/4 1/4 1/4 1/4]
+               [  0   0   1   2])
+       (then
+        (phrase [1/4 1/4 1/4 2/12 1/12]
+                [  2   1   0   0 1]))
+       (then
+        (phrase [1/4 1/4 1/4 1/4]
+                [  2   2   -3   0]))
+       (then
+        (phrase [3/8 1/8 2/4]
+                [  0   -1   -1]))
+    ;; (then
+    ;;            ; Gently down the stream,
+    ;;    (phrase [2/3 1/3 2/3 1/3 6/3]
+    ;;            [  2   1   2   3   4]))
+    ;; (then
+    ;;            ; Merrily, merrily, merrily, merrily,
+    ;;    (phrase (repeat 12 1/3) 
+    ;;            (mapcat (partial repeat 3) [7 4 2 0])))
+    ;; (then
+    ;;            ; Life is but a dream!
+    ;;    (phrase [2/3 1/3 2/3 1/3 6/3] 
+    ;;            [  4   3   2   1   0]))
+    (where :part (is :alto1))))
 
 (def bass "A bass part to accompany the melody."
   (->> (phrase [1  1 2]
@@ -116,6 +149,7 @@
   [speed key]
   (->> melody-ode
     (with countermelody-ode)
+    (with countermelody-ode-alto2)
     (times 1)
       (where :time speed)
     (where :duration speed)
